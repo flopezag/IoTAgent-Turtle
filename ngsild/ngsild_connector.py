5dd1379414626d7dd4f898dea41b854b0a1be63b
Jose Ignacio Carretero <joseignacio.carretero@gmail.com>
2023-04-06 13:25:27 +0200

    solved CB writings

--- ngsild/ngsild_connector.py
+++ ngsild/ngsild_connector.py
@@ -15,23 +15,21 @@ class NGSILDConnector:
         self.base_url = config['broker']
 
     def get_url(self):
-        url = f"{self.base_url}/ngsi-ld/v1/entityOperations/create"
+        url = f"{self.base_url}/ngsi-ld/v1/entities"
         return url
 
-    def send_pretty_much_data(self, json_object):
+    def send_data_array(self, json_object):
         d = json.loads(json_object)
         d = d if type(d) is list else [d]
 
         for elem in d:
             rc, r = c.send_data(json.dumps(elem))
-            print("Code: ", rc)
-            print(r)
 
     def send_data(self, json_object):
         # Send the data to a FIWARE Context Broker instance
         headers = {
-            'Content-Type': 'application/ld+json',
-            'Accept': 'application/ld+json'
+            'Content-Type': 'application/ld+json'
+            #, 'Accept': 'application/ld+json'
         }
 
         url = self.get_url()
@@ -39,8 +37,11 @@ class NGSILDConnector:
 
         r = post(url=url, headers=headers, data=json_object, timeout=5)
 
-        resp = json.loads(r.text)
+        # resp = json.loads(r.text)
         response_status_code = r.status_code
+
+        if response_status_code == 201:
+            print("LOCATION: ", r.headers['Location'])
 
         # Let exceptions raise.... They can be controlled somewhere else.
         return response_status_code, resp
@@ -58,12 +59,4 @@ if __name__ == "__main__":
         rdf_data = rf.read()
 
     r = parser.parsing(StringIO(rdf_data), out=False)
-
-    with open("/tmp/ww", "w") as f:
-        f.write(r)
-
-    # print(data)
-    c.send_pretty_much_data(r)
-    # rc, r = c.send_data(data)
-    # print("Code: ", rc)
-    # print(r)
+    c.send_data_array(r)
